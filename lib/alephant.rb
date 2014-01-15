6329c63c51fc8bd4ff565e21d24f830aa9a30550
Robert Kenny <kenoir@gmail.com>
2014-01-15 18:03:15 +0000

    created alephantrunner

--- lib/alephant.rb
+++ lib/alephant.rb
@@ -12,31 +12,31 @@ require 'alephant/models/sequencer'
 require 'alephant/errors'
 require 'alephant/views'
 
-
 module Alephant
+  class AlephantRunner
+    attr_reader :queue, :cache, :sequencer, :renderer, :cache_path
 
-  def self.run(cache_id)
-    queue = AWS::SQS.new.queues.create(cache_id)
-    cache = Cache.new(cache_id)
-    sequencer = Sequencer.new(cache_id)
-    renderer = Renderer.new(cache_id)
+    def initialize(opts = {})
+      opts.each { |k,v| instance_variable_set("@#{k}",v) }
+    end
 
-    thread = Thread.new do
-      puts "Polling queue..."
-      queue.poll do |msg|
-        data = JSON.parse(msg.body)
+    def run!
+      #queue = AWS::SQS.new.queues.create(cache_id)
+      #cache = Cache.new(cache_id,cache_id)
+      #sequencer = Sequencer.new(cache_id)
+      #renderer = Renderer.new(cache_id)
 
-        if data["seq"] > sequencer.last_seen
-          puts "Rendering from message #{data["seq"]}"
-
-          content = renderer.render(data)
-          cache.put(cache_id, content)
-          sequencer.last_seen = data["seq"]
+      Thread.new do
+        @queue.poll do |msg|
+          data = JSON.parse(msg.body)
+          current = @sequencer.sequence(data)
+          if current > @sequencer.last_seen
+            content = @renderer.render(data)
+            @cache.put(@cache_path, content)
+            @sequencer.last_seen = current
+          end
         end
       end
     end
-
-    thread
   end
 end
-
