e3bb5167a673e757370b0ec802db3efa5b05cbc4
Robert Kenny <kenoir@gmail.com>
2014-01-16 16:05:56 +0000

    adds spec for poll

--- spec/queue_spec.rb
+++ spec/queue_spec.rb
@@ -3,6 +3,19 @@ require 'spec_helper'
 describe Alephant::Queue do
   subject { Alephant::Queue }
 
+  describe "poll(*args, &block)" do
+    it "calls @q.poll(*args, &block)" do
+      block = double()
+
+      AWS::SQS::Queue.any_instance.stub(:exists?)
+        .and_return(true)
+
+      AWS::SQS::Queue.any_instance.should_receive(:poll).with(:arg)
+
+      subject.new(:id).poll(:arg)
+    end
+  end
+
   describe "sleep_until_queue_exists" do
     context "@q.exists? == true" do
       it "should not call sleep" do
