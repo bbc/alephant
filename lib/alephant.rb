77786e7405a0565475845633b5295248a5b42ff1
Robert Kenny <kenoir@gmail.com>
2014-01-16 04:43:39 +0000

    midnight mega refactor

--- lib/alephant.rb
+++ lib/alephant.rb
@@ -5,6 +5,7 @@ require 'json'
 
 require_relative 'env'
 
+require 'alephant/models/queue'
 require 'alephant/models/cache'
 require 'alephant/models/renderer'
 require 'alephant/models/sequencer'
@@ -14,26 +15,35 @@ require 'alephant/views'
 
 module Alephant
   class AlephantRunner
-    attr_reader :queue, :cache, :sequencer, :renderer, :cache_path
+    attr_accessor :queue, :cache, :sequencer, :renderer
+    attr_accessor :s3_bucket_id, :s3_object_path, :table_name, :sqs_queue_id, :view_id
 
     def initialize(opts = {})
       opts.each { |k,v| instance_variable_set("@#{k}",v) }
+
+      @sequencer ||= Sequencer.new(
+        {
+          :table_name => @table_name
+        },
+        @sqs_queue_id
+      )
+
+      @queue ||= Queue.new(@sqs_queue_id)
+      @cache ||= Cache.new(@s3_bucket_id)
+      @renderer ||= Renderer.new(@view_id)
     end
 
-    def run!
-      #queue = AWS::SQS.new.queues.create(cache_id)
-      #cache = Cache.new(cache_id,cache_id)
-      #sequencer = Sequencer.new(cache_id)
-      #renderer = Renderer.new(cache_id)
-
+    def run!(sequential_proc = nil, set_last_seen_proc = nil)
       Thread.new do
         @queue.poll do |msg|
           data = JSON.parse(msg.body)
-          current = @sequencer.sequence(data)
-          if current > @sequencer.last_seen
-            content = @renderer.render(data)
-            @cache.put(@cache_path, content)
-            @sequencer.last_seen = current
+
+          if @sequencer.sequential?(data, &sequential_proc)
+            @cache.put(
+              @s3_object_path,
+              @renderer.render(data)
+            )
+            @sequencer.set_last_seen(data, &set_last_seen_proc)
           end
         end
       end
---
+++ lib/alephant/models/queue.rb
@@ -0,0 +1,19 @@
+require 'aws-sdk'
+
+module Alephant
+  class Queue
+    attr_accessor :queue
+
+    def initialize(id)
+      @sqs = AWS::SQS.new
+      @q = @sqs.queues[id].exists?
+      sqs.queues
+      @queue = AWS::SQS.new.queues.create(id)
+    end
+
+    def sleep_until_queue_exists
+      sleep 1 until @q.exists?
+    end
+
+  end
+end
