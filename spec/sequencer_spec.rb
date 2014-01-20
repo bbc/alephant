1f468b0cfbc904baff729b639c510c835f26c0a5
Robert Kenny <kenoir@gmail.com>
2014-01-20 08:21:49 +0000

    start adding tests

--- spec/sequencer_spec.rb
+++ spec/sequencer_spec.rb
@@ -3,63 +3,32 @@ require 'spec_helper'
 describe Alephant::Sequencer do
   subject { Alephant::Sequencer }
 
-  describe "initialize(opts, id)" do
-    it "sets @id, @table_name and @table_conf" do
-      sleep_until_table_active = double()
-
-      Alephant::Sequencer.any_instance.stub(:sleep_until_table_active).and_return(sleep_until_table_active);
-
-      AWS::DynamoDB.any_instance.stub(:initialize).and_return({
-        :tables => :table_name
-      })
-
-      instance = subject.new({
-        :table_name => :table_name,
-        :table_conf => :table_conf
-      }, :sqs_queue_id)
-
-      expect(instance.id).to eq(:sqs_queue_id)
-      expect(instance.table_name).to eq(:table_name)
-      expect(instance.table_conf).to eq(:table_conf)
-    end
-
-    it "calls sleep_until_table_active() until table created" do
-      pending
-    end
-  end
-
   describe "sequential?(data)" do
-    before(:each) do
-      Alephant::Sequencer
-        .any_instance.stub(:initialize)
-        .and_return(double())
-    end
 
-    context "block provided" do
-      it "yields to block" do
-        Alephant::Sequencer
-          .any_instance
-          .stub(:get_last_seen)
-          .and_return(1)
+    context "block_given? == true" do
 
-        instance = subject.new
-
-        test = instance.sequential?(:data) do |last_seen, data|
-          :foo
+      context "block is { herp }" do
+        it "should fail" do
+          fail
         end
-
-        expect(test).to eq(:foo)
       end
+
     end
 
-    context "no block provided" do
-      it "returns true if message is newer than get_last_seen()" do
-        pending
+    context "block_given? == false" do
+
+      context "get_last_seen < data[:sequence_id]" do
+        it "should fail" do
+          fail
+        end
       end
 
-      it "returns false if message is older than get_last_seen()" do
-        pending
+      context "get_last_seen >= data[:sequence_id]" do
+        it "should fail" do
+          fail
+        end
       end
+
     end
   end
 end
