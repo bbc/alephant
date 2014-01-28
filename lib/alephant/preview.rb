f0a7750d9418aa3d83ee89f45d28292e00c9a90c
Robert Kenny <kenoir@gmail.com>
2014-01-28 09:42:31 +0000

    adds component preview path

--- lib/alephant/preview.rb
+++ lib/alephant/preview.rb
@@ -1,10 +1,41 @@
 require 'sinatra/base'
+require 'alephant/models/renderer'
+require 'json'
+
 
 module Alephant
   class Preview < Sinatra::Base
-    get '/component/:id/preview' do
-      'hello world'
+    get '/preview/:id/:fixture?' do
+      "preview"
     end
 
+    get '/component/:id/:fixture?' do
+      render_component
+    end
+
+    def render_component
+      Renderer.new(id, base_path).render(fixture_data)
+    end
+
+    private
+    def id
+      params['id']
+    end
+
+    def fixture
+      params['fixture'] || params['id']
+    end
+
+    def fixture_data
+      JSON.parse(File.open(fixture_location).read)
+    end
+
+    def base_path
+      "#{Dir.pwd}/views"
+    end
+
+    def fixture_location
+      "#{base_path}/fixtures/#{fixture}.json"
+    end
   end
 end
