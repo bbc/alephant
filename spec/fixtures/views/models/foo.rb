fda62192a87e629ab15c85025862f28e33ec41b9
Integralist <mark.mcdx@gmail.com>
2014-02-03 12:08:09 +0000

    Moved up tests from renderer into multi_renderer

---
+++ spec/fixtures/views/models/foo.rb
@@ -0,0 +1,7 @@
+module MyApp
+  class Foo < ::Alephant::Views::Base
+    def content
+      "content"
+    end
+  end
+end
--- spec/multi_renderer_spec.rb
+++ spec/multi_renderer_spec.rb
@@ -1,7 +1,31 @@
 require 'spec_helper'
 
 describe Alephant::MultiRenderer do
+  let(:model_file) { 'foo' }
   subject { Alephant::MultiRenderer }
+
+  before(:each) do
+      @instance = subject.new(model_file)
+      @instance.base_path = File.join(
+        File.dirname(__FILE__),
+        'fixtures',
+        'views'
+      )
+  end
+
+  describe "initialize(view_base_path)" do
+    context "view_base_path = nil" do
+      it "sets base_path" do
+        expect(subject.new(model_file).base_path).to eq('views')
+      end
+    end
+
+    context "view_base_path = '.'" do
+      it "sets base_path" do
+        expect(subject.new(model_file, '.').base_path).to eq('.')
+      end
+    end
+  end
 
   describe "render(data)" do
     it "calls ::Alephant::renderer.render() for each template found" do
@@ -10,17 +34,31 @@ describe Alephant::MultiRenderer do
         :bar => 'content'
       }
 
-      instance = subject.new
-
-      instance.base_path = File.join(
-        File.dirname(__FILE__),
-        'fixtures',
-        'views'
-      )
-
-      content = instance.render({ :foo => :bar })
+      content = @instance.render({ :foo => :bar })
       content.each do |template_type, rendered_content|
         expect(rendered_content).to eq(templates[template_type])
+      end
+    end
+  end
+
+  describe "model(data)" do
+    let(:data) {{ :key => :value }}
+
+    it "returns the model" do
+      model = @instance.model(data)
+      model.should be_an Alephant::Views::Base
+      expect(model.data).to eq(data)
+    end
+
+    context "invalid model" do
+      it 'should raise ViewModelNotFound' do
+        instance = subject.new('invalid_model_file', @base_path)
+
+        expect {
+          instance.model(data)
+        }.to raise_error(
+          Alephant::Errors::ViewModelNotFound
+        )
       end
     end
   end
