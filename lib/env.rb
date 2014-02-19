bd69936f42d504d84317d93e18c6976c3650c90b
Robert Kenny <kenoir@gmail.com>
2014-02-19 11:10:36 +0000

    remove reference to util string

--- lib/env.rb
+++ lib/env.rb
@@ -3,8 +3,6 @@ $LOAD_PATH << File.dirname(__FILE__)
 require 'aws-sdk'
 require 'yaml'
 
-require 'alephant/util/string'
-
 config_file = File.join('config', 'aws.yaml')
 
 if File.exists? config_file
