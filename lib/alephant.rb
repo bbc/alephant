5fbbf6a6eb3d2ed1ef629d04bbd50fadd1aa4d26
Steven Jack <stevenmajack@gmail.com>
2014-06-05 12:12:58 +0100

    Adds 'around' method to AOP module to allow consumers to overwrite methods in classes that extend Alephant::AOP

--- lib/alephant.rb
+++ lib/alephant.rb
@@ -3,4 +3,20 @@ require "alephant/publisher"
 
 module Alephant
   # Consuming application calls: `Alephant::Publisher.create`
+
+  module AOP
+
+    def around(fn_name)
+      puts 'In around method'
+      old_method = instance_method(fn_name)
+      define_method(fn_name) do |*args|
+        yield :before, args if block_given?
+        old_method.bind(self).call(args).tap do |return_value|
+          args << return_value unless return_value.nil?
+        end
+        yield :after, args if block_given?
+      end
+    end
+
+  end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,3 @@
+require 'pry'
+require_relative 'fixtures/test'
+
