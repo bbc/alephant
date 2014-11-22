5fd55b00f618b87c54f3de56bb98533a0fd2b853
Integralist <mark.mcdx@gmail.com>
2014-11-22 10:03:40 +0000

    Tweaks to Sender application to allow renderer of different component types

--- Example/Sender/app.rb
+++ Example/Sender/app.rb
@@ -4,8 +4,13 @@ require "env"
 
 class App
   def self.message_generator
+    set_components
     configure_queue
     loop { send_message }
+  end
+
+  def self.set_components
+    @@components = ["test", "foo", "bar"]
   end
 
   def self.configure_queue
@@ -16,11 +21,19 @@ class App
     time = Random.rand(10..30)
     p "Going to sleep for #{time} seconds before sending a message to the queue"
     sleep time # mimic messages coming into queue at random
-    queue.send_message(message)
+    queue.send_message(message) unless @@components.empty?
   end
 
   def self.message
-    { :foo => "bar", :timestamp => Time.now.utc }.to_json
+    { :component => component, :title => "My Title", :timestamp => Time.now.utc }.to_json
+  end
+
+  def self.component
+    @@components.shift
+
+    # we wouldn't do this in a real application
+    # this code just makes it easier for us to
+    # render (theorectically) different templates
   end
 
   def self.queue
