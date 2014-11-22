0932c7173e4116b2c70004f3febffa222cabc4ee
Integralist <mark.mcdx@gmail.com>
2014-11-22 10:46:38 +0000

    Add Broker application

---
+++ Example/Broker/env.rb
@@ -0,0 +1,16 @@
+require "aws-sdk"
+require "dotenv"
+
+ENV["APP_ENV"] = ENV.fetch("APP_ENV", "development")
+
+if ENV["APP_ENV"] == "development"
+  require "pry"
+  require "spurious/ruby/awssdk/helper"
+  Spurious::Ruby::Awssdk::Helper.configure
+end
+
+Dotenv.load(
+  File.join(
+    File.dirname(__FILE__), "config", ENV["APP_ENV"], "env.yaml"
+  )
+)
