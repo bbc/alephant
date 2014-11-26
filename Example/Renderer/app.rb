b0ceed267e14b8b1a0b2f1108c4fb8f818c0cd36
Steven Jack <stevenmajack@gmail.com>
2014-11-26 09:56:42 +0000

    Private methods

--- Example/Renderer/app.rb
+++ Example/Renderer/app.rb
@@ -5,6 +5,19 @@ require "alephant/logger"
 require_relative "env"
 
 module App
+  include Alephant::Logger
+
+  def self.run!
+    loop do
+      begin
+        Alephant::Publisher::Queue.create(options).run!
+      rescue Exception => e
+        logger.warn "Error: #{e.message}\n#{e.backtrace.join("\n")}"
+      end
+    end
+  end
+
+  private
 
   def self.app_config
     BBC::Cosmos::Config.app
@@ -21,26 +34,14 @@ module App
       )
 
       opts.add_writer(
-        :sequencer_table_name => ENV["SEQUENCER_TABLE_NAME"],
-        :lookup_table_name    => ENV["LOOKUP_TABLE_NAME"],
+        :sequencer_table_name => ENV['SEQUENCER_TABLE_NAME'],
+        :lookup_table_name    => ENV['LOOKUP_TABLE_NAME'],
         :sequence_id_path     => ENV["SEQUENCE_ID_PATH"],
         :renderer_id          => ENV["RENDERER_ID"],
         :s3_bucket_id         => ENV["S3_BUCKET_ID"],
         :s3_object_path       => ENV["S3_OBJECT_PATH"],
         :view_path            => component_base_path
       )
-    end
-  end
-
-  def self.run!
-    loop do
-      begin
-        Alephant::Publisher::Queue.create(options).run!
-      rescue Exception => e
-        error_message = "#{e.message}\n#{e.backtrace.join("\n")}"
-
-        Alephant::Logger.get_logger.warn "Error: #{error_message}"
-      end
     end
   end
 end
