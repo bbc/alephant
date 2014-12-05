3ab68ca1ed93a4488626d454f4b74f94869febf5
Charlie Revett <charlierevett@gmail.com>
2014-12-05 15:33:42 +0000

    env.rb

---
+++ Example/Request-based/Renderer/env.rb
@@ -0,0 +1,20 @@
+require "aws-sdk"
+require "dotenv"
+
+ENV["APP_ENV"]      = ENV.fetch("APP_ENV", "development")
+ENV["DEV_APP_CONF"] = File.join(File.dirname(__FILE__), 'config', 'development', 'app.json')
+
+if ENV["APP_ENV"] == "development"
+  require "pry"
+  require "spurious/ruby/awssdk/helper"
+  Spurious::Ruby::Awssdk::Helper.configure
+end
+
+Dotenv.load(
+  File.join(
+    File.dirname(__FILE__),
+    "config",
+    ENV["APP_ENV"],
+    "env.yaml"
+  )
+)
