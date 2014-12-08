b3d85e4e1601e98fb1fa45c3affb0f6ac24f1b9d
Charlie Revett <charlierevett@gmail.com>
2014-12-08 17:03:00 +0000

    Use routing hash as Dynamo key

--- Example/Request-based/Broker/lib/broker/dynamic_url_locator.rb
+++ Example/Request-based/Broker/lib/broker/dynamic_url_locator.rb
@@ -9,7 +9,7 @@ module Broker
     end
 
     def generate(component_id, options)
-      location_from query(component_id, options)
+      "#{location_from query(options)}/component/#{component_id}"
     end
 
     private
@@ -20,41 +20,41 @@ module Broker
       config[:dynamo_table_name]
     end
 
-    def location_from(results)
-      results[:count] == 1 ? results[:member].first['location'][:s]
-                           : 'http://google.com'
-    end
-
-    def query(component_id, options)
-      dynamo.query query_options(
-        component_id,
-        Crimp.signature(options)
+    def routing_from(options)
+      Crimp.signature(
+        options[:routing] || {}
       )
     end
 
-    def query_options(component_id, opts_hash)
+    def location_from(results)
+      results[:count] == 1 ? results[:member].first['location'][:s]
+                           : raise(Broker::DynamicUrlLocator::NotFound)
+    end
+
+    def query(options)
+      dynamo.query query_options(
+        routing_from(options)
+      )
+    end
+
+    def query_options(opts_hash)
       {
         :attributes_to_get => [
           'location'
         ],
         :key_conditions => {
-          'component_id' => {
-            :attribute_value_list => [
-              { 's' => opts_hash }
-            ],
-            :comparison_operator => 'EQ'
-          },
           'opts_hash' => {
             :attribute_value_list => [
-              { 's' => component_id }
+              { 's' => opts_hash }
             ],
             :comparison_operator => 'EQ'
           }
         },
         :limit => 1,
-        :select => 'SPECIFIC_ATTRIBUTES',
         :table_name => dynamo_table_name
       }
     end
+
+    class NotFound < Exception; end
   end
 end
